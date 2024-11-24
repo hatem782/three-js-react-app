c3a51c301b40bf82fec55369dce6e1b8861457ed
hatem782 <hatembenechikh100@gmail.com>
2024-11-24 17:09:38 +0100

    road and car

---
+++ src/Scenes/City/Road.tsx
@@ -0,0 +1,195 @@
+import React from "react";
+import Model3D from "../../Components/Model3D/Model3D";
+
+function Road() {
+  return (
+    <>
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: -30, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 60, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 120, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 120, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 90, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 60, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: 30, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-plain-grass.glb"
+        position={{ x: -30, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 0, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 30, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 270, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 30, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-straight.glb"
+        position={{ x: 0, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 180, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-straight.glb"
+        position={{ x: 0, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 180, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-straight.glb"
+        position={{ x: 60, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-intersection.glb"
+        position={{ x: 90, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-intersection.glb"
+        position={{ x: 0, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-straight.glb"
+        position={{ x: 30, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-straight.glb"
+        position={{ x: 60, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 90, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 180, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 0, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 180, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: -30, y: 0, z: 90 }}
+        rotation={{ x: 0, y: 90, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: -30, y: 0, z: 60 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 120, y: 0, z: 30 }}
+        rotation={{ x: 0, y: 180, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 90, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 0, z: 0 }}
+        scale={1}
+      />
+      <Model3D
+        key="3"
+        src="/models/tiles/tile-mainroad-curve.glb"
+        position={{ x: 120, y: 0, z: 0 }}
+        rotation={{ x: 0, y: 270, z: 0 }}
+        scale={1}
+      />
+    </>
+  );
+}
+
+export default Road;
